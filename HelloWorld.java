c5619c4feb0e475a9d8a76be987e8bcde0de3e87
zhangyang <zhangyang_2002@foxmail.com>
2023-08-05 17:11:08 +0800

    demo成功

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
+public class HelloWorld {
+    public HelloWorld() {
+    }
+
+    public static void main(String[] var0) {
+        System.out.println("Hello, World!");
+    }
+}
---
+++ README.md
@@ -0,0 +1,12 @@
+在线笔记:https://www.yuque.com/zhangyang.com/tuiv0m/eitgt63cqhgegyg2
+windows执行demo前置
+1. GraalVM
+2. windows10 sdk
+3. msvc
+需要安装并配置环境变量
+通过gu下载native-image
+执行命令
+```text
+javac HelloWorld.java
+native-image HelloWorld
+```
